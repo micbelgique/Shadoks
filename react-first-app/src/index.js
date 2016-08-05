c32b06fa37fb8b656e0fd802219b17e2ce097a0f
Mathieu Wautier <mathieu.wautier@idweaver.com>
2016-08-05 23:17:04 +0200

    react first app test

---
+++ react-first-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './App';
+import './index.css';
+
+ReactDOM.render(
+  <CommentBox/>,
+  document.getElementById('root')
+);
