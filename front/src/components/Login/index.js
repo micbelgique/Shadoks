031cea91cccc8f006fb74222a35faa395e42e0f9
Vincent Margan <Vincent Margan>
2016-08-07 00:11:27 +0200

    no message

--- front/src/components/Login/index.js
+++ front/src/components/Login/index.js
@@ -1,39 +1,59 @@
 import React, {Component} from 'react';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../../theme/css/AdminLTE.css';
+import '../../theme/css/skins/_all-skins.css';
 
-class Login extends Component{
-    render(){
+class Login extends Component {
+    render() {
         return (
-            <div class="col-md-6">
-                <div class="box box-info">
-                    <div class="box-header with-border">
-                        <h3>Veuillez introduire vos infos de connection</h3>
+            <div className="hold-transition login-page">
+                <div className="login-box">
+                    <div className="login-logo">
+                        <a href="../../index2.html">
+                            <b>Co</b>Workers</a>
                     </div>
-                    <form class="form-horizontal">
-                        <div class="box-body">
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label" for="Login">
-                                    Login
-                                </label>
-                                <div class="col-sm-10">
-                                    <input type="text" id="Login" />
-                                </div>
+
+                    <div className="login-box-body">
+                        <p className="login-box-msg">Sign in to start your session</p>
+
+                        <form action="../../index2.html" method="post">
+                            <div className="form-group has-feedback">
+                                <input type="email" className="form-control" placeholder="Email"/>
+                                <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
                             </div>
-                            <div class="form-group">
-                                <label class="col-sm-2 control-label" for="Password">
-                                    Password
-                                </label>
-                                <div class="col-sm-10">
-                                    <input type="Password" id="Password" />
-                                </div>
+                            <div className="form-group has-feedback">
+                                <input type="password" className="form-control" placeholder="Password"/>
+                                <span className="glyphicon glyphicon-lock form-control-feedback"></span>
                             </div>
+                            <div className="row">
+                                <div className="col-xs-offset-8 col-xs-4">
+                                    <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
+                                </div>
+
+                            </div>
+                        </form>
+
+                        <div className="social-auth-links text-center">
+                            <p>- OR -</p>
+                            <a href="#" className="btn btn-block btn-social btn-facebook btn-flat">
+                                <i className="fa fa-facebook"></i>
+                                Sign in using Facebook</a>
+                            <a href="#" className="btn btn-block btn-social btn-google btn-flat">
+                                <i className="fa fa-google-plus"></i>
+                                Sign in using Google+</a>
                         </div>
-                    </form>
+
+                        <a href="#">I forgot my password</a><br/>
+                        <a href="register.html" className="text-center">Register a new membership</a>
+
+                    </div>
                 </div>
             </div>
         )
     }
 
 }
-
 
 export default Login;
